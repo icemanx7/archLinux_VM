b55768a07026bfcb01bdf29d530bfa10868a6fa0
Richard.Thomas <richard.thomas@localhost.localdomain>
2016-01-11 08:28:41 +0200

    added btrfs support

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ This is a script to quickly install arch linux for virtualbox. It adds the Vbox 
 To run this either copy it onto the install media using another pc with ssh and scp or rebuild the arch linux iso with this script located on it. Follow the Arch Wiki guide Remastering the iso. 
 I can verify that it works. 
 
+## Added a part 2 to the repo
+
+This adds a exact copy but makes the files system btrfs instead of ext4 for
+better speed and snapshot support.
+
 ### TIPS
 
 This works very nice with the virtualbox creatitition from terminal. Which I
---
+++ part2.sh
@@ -0,0 +1,33 @@
+#parted /dev/sda "mklabel msdos yes"
+#parted /dev/sda "mkpart primary ext4 1MiB 100%"
+#parted /dev/sda "set 1 boot on"
+
+#mkfs.btrfs /dev/sda1
+mount /dev/sda1 /mnt 
+#pacstrap -i /mnt base base-devel
+genfstab -U /mnt > /mnt/etc/fstab
+
+#TODO
+#This will probably be a problem for you when since it is only english. Working on a better solution with user interaction.
+cp /etc/locale.gen /mnt/etc/
+arch-chroot /mnt /bin/bash "locale-gen"
+echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
+arch-chroot /mnt /usr/bin/ln -s /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+arch-chroot /mnt mkinitcpio -p linux
+arch-chroot /mnt pacman -S --noconfirm grub
+arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt echo virtual > /etc/hostname
+arch-chroot /mnt pacman -S --noconfirm networkmanager
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt pacman -S --noconfirm virtualbox-guest-utils
+arch-chroot /mnt modprobe -a vboxguest vboxsf vboxvideo
+arch-chroot /mnt systemctl enable vboxservice
+arch-chroot /mnt pacman -S --noconfirm xfce4
+
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash 
+
+
+
+
+
